42b392a149f27f4993e72e672d066c082097739e
JohannGaviria <johannmgr@gmail.com>
2024-11-04 22:01:30 -0500

    feat(surveys): crear endpoint para buscar encuestas

--- apps/surveys/urls.py
+++ apps/surveys/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('surveys/create', views.create_survey, name='create_survey'),
     path('surveys/get/<str:survey_id>', views.get_survey_id, name='get_survey_id'),
     path('surveys/get_all', views.get_all_surveys, name='get_all_surveys'),
+    path('surveys/search_survey', views.search_surveys, name='search_surveys'),
 ]
