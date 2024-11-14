a2a7eea48626417a4448b7cd5adfc51210fc54dd
JohannGaviria <johannmgr@gmail.com>
2024-11-14 11:23:54 -0500

    feat(surveys): crear endpoint para la actualizacion de encuestas

--- apps/surveys/urls.py
+++ apps/surveys/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('surveys/get/<str:survey_id>', views.get_survey_id, name='get_survey_id'),
     path('surveys/get_all', views.get_all_surveys, name='get_all_surveys'),
     path('surveys/search_survey', views.search_surveys, name='search_surveys'),
+    path('surveys/update/<str:survey_id>', views.update_survey, name='update_survey'),
     path('surveys/delete/<str:survey_id>', views.delete_survey, name='delete_survey'),
 ]
