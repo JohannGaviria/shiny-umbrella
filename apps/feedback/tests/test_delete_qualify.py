4272d8793368869deaa8b3605c15bd6a025d57ba
JohannGaviria <johannmgr@gmail.com>
2024-12-03 17:42:48 -0500

    tests(feedbacks): crear tests para el endpoint de delete qualify survey

---
+++ apps/feedback/tests/test_delete_qualify.py
@@ -0,0 +1,100 @@
+from rest_framework.test import APIClient
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.utils import timezone
+from apps.surveys.models import Survey
+from apps.feedback.models import Qualify
+from faker import Faker
+from datetime import timedelta
+import random
+
+
+fake = Faker()
+
+
+# Tests para eliminar una calificación de una encuesta
+class DeleteQualifySurveyTestsCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create(
+            username='TestUsername',
+            email='test@email.com',
+        )
+        self.user.set_password('TestPassword')
+        self.user.save()
+        self.token = Token.objects.create(user=self.user)
+        self.client.force_authenticate(user=self.user)
+        self.user_not_create = User.objects.create(
+            username=fake.user_name(),
+            email=fake.email(),
+        )
+        self.user_not_create.set_password(fake.password())
+        self.user_not_create.save()
+        self.survey = Survey.objects.create(
+            title=fake.sentence(nb_words=6),
+            description=fake.text(max_nb_chars=200),
+            end_date=timezone.now() + timedelta(days=random.randint(1, 30)),
+            is_public=True,
+            user=self.user
+        )
+        self.qualify = Qualify.objects.create(
+            assessment=fake.random_int(min=1, max=5),
+            user=self.user,
+            survey=self.survey
+        )
+        self.url = reverse('delete_qualify_survey', args=[self.survey.id, self.qualify.id])
+
+    
+    def test_delete_qualify_survey_successfully(self):
+        """
+        Prueba de eliminar una calificación de una encuesta exitosamente.
+        """
+        response = self.client.delete(self.url, format='josn')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue('status' in response.data)
+        self.assertTrue('message' in response.data)
+
+
+    def test_delete_qualify_survey_not_found(self):
+        """
+        Prueba de eliminar una calificación de una encuesta que no existe.
+        """
+        url = reverse('delete_qualify_survey', args=['ec7f051a960d42788a10ff51cc85dc23', self.qualify.id])
+        response = self.client.delete(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertTrue('status' in response.data)
+        self.assertTrue('message' in response.data)
+
+    
+    def test_delete_qualify_survey_qualify_not_found(self):
+        """
+        Prueba de eliminar una calificación que no existe de una encuesta.
+        """
+        url = reverse('delete_qualify_survey', args=[self.survey.id, 9999])
+        response = self.client.delete(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertTrue('status' in response.data)
+        self.assertTrue('message' in response.data)
+    
+
+    def test_delete_qualify_survey_user_is_not_create(self):
+        """
+        Prueba de eliminar una calificación de una encuesta que el usuario no es el creador.
+        """
+        self.client.force_authenticate(user=self.user_not_create)
+        response = self.client.delete(self.url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertTrue('status' in response.data)
+        self.assertTrue('message' in response.data)
+    
+
+    def test_delete_qualify_survey_without_token(self):
+        """
+        Prueba de eliminar una calificación de una encuesta sin token.
+        """
+        self.client.force_authenticate(user=None)
+        response = self.client.delete(self.url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
