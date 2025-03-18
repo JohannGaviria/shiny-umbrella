63bf830b0f8111d6d879113b8064a032efdfb00c
Johann Gaviria <johannmgr@gmail.com>
2025-03-18 16:55:57 -0500

    feat(docker): actualizar la dockerizacion del proyecto

--- config/settings/development.py
+++ config/settings/development.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'G$9dks!Bf8zQ3*wL%mH7RnX#1p@Vk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ '*' ]
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -11,4 +11,4 @@ echo 'Creating staticfiles...'
 python manage.py collectstatic --no-input --settings=config.settings.production
 
 echo 'Running server...'
-gunicorn --env DJANGO_SETTINGS_MODULE=config.settings.production config.wsgi:application --bind 0.0.0.0:8000
+gunicorn --env DJANGO_SETTINGS_MODULE=config.settings.production config.wsgi:application --bind 0.0.0.0:$PORT
